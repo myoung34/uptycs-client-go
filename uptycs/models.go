130655b41e5eed7601aaa9ea77edfad37f0f9812
Marcus Young <myoung34@my.apsu.edu>
2022-08-17 13:04:16 -0500

    Test fixing booleans disappearing from eventrule builderconfig json body

--- uptycs/models.go
+++ uptycs/models.go
@@ -58,14 +58,14 @@ type EventRule struct {
 type BuilderConfigFilter struct {
 	And             []BuilderConfigFilter `json:"and,omitempty"`
 	Or              []BuilderConfigFilter `json:"or,omitempty"`
-	Not             bool                  `json:"not,omitempty"`
+	Not             bool                  `json:"not"`
 	Name            string                `json:"name,omitempty"`
 	Value           ArrayOrString         `json:"value,omitempty"`
 	Operator        string                `json:"operator,omitempty"`
-	IsDate          bool                  `json:"isDate,omitempty"`
-	IsVersion       bool                  `json:"isVersion,omitempty"`
-	IsWordMatch     bool                  `json:"isWordMatch,omitempty"`
-	CaseInsensitive bool                  `json:"caseInsensitive,omitempty"`
+	IsDate          bool                  `json:"isDate"`
+	IsVersion       bool                  `json:"isVersion"`
+	IsWordMatch     bool                  `json:"isWordMatch"`
+	CaseInsensitive bool                  `json:"caseInsensitive"`
 }
 
 type ArrayOrString []string
